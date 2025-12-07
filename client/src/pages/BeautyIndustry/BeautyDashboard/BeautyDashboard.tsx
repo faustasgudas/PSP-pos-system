77e2c61828c8825b0ef0a3526f5a1ebc9ec75169
prupletea <prupletea@outlook.com>
2025-12-07 19:02:10 +0200

    fix typing error

--- client/src/pages/BeautyIndustry/BeautyDashboard/BeautyDashboard.tsx
+++ client/src/pages/BeautyIndustry/BeautyDashboard/BeautyDashboard.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import "../../../App.css"
-import "./BeautyDashboard.css.css"
+import "./BeautyDashboard.css"
 import BeautyReservations from "../BeautyReservations/BeautyReservations";
 
 // Type definitions
