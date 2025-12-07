ce72ad63ebeb0afa4922b0627d1797a8c0b24304
Matas <m.firantas@gmail.com>
2025-12-07 16:55:16 +0200

    migrations

---
+++ PsP/Migrations/20251207141139_ADDEDcatalogitemType.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PsP.Migrations
+{
+    /// <inheritdoc />
+    public partial class ADDEDcatalogitemType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CatalogTypeSnapshot",
+                table: "OrderLines",
+                type: "character varying(16)",
+                maxLength: 16,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CatalogTypeSnapshot",
+                table: "OrderLines");
+        }
+    }
+}
