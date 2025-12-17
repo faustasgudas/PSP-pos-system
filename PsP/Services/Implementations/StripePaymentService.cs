c4ce65549889c7a684ba6626ed37f3f71c8eaeb0
Ignė Balvočiūtė <142167949+ignebal@users.noreply.github.com>
2025-12-17 19:23:48 +0200

    Squashed commit of the following:
    commit 2ecaaeade706c067eece130254ecf5412d8d509c
    Author: faustasgudas <faustas.gudas@gmail.com>
    Date:   Wed Dec 17 15:46:51 2025 +0200
    
        fix order status after refund
    
    commit c5ad2f598297421587f29ca278fc59aa946c4bdf
    Author: faustasgudas <faustas.gudas@gmail.com>
    Date:   Wed Dec 17 15:41:42 2025 +0200
    
        refunds work
    
    commit b4b3f7ab940c6d7df29733a29211ff93f1cbc9af
    Merge: 73b9f99 b6495e1
    Author: Matas <m.firantas@gmail.com>
    Date:   Wed Dec 17 15:02:36 2025 +0200
    
        Merge remote-tracking branch 'origin/Yolo' into Yolo
    
    commit 73b9f997662a3aba093b720fd29a834b8faacd23
    Author: Matas <m.firantas@gmail.com>
    Date:   Wed Dec 17 15:02:19 2025 +0200
    
        EMPLOYES GET PAKOREGUOTAS
    
    commit b6495e164d2de69488e680a106f3889f83bdf415
    Author: faustasgudas <faustas.gudas@gmail.com>
    Date:   Wed Dec 17 14:57:54 2025 +0200
    
        booking Employee select disable for staff
    
    commit e647c4a0adbaf12bcd003c87692aa3026c9f554f
    Merge: d683cf4 ed3c11e
    Author: Matas <m.firantas@gmail.com>
    Date:   Wed Dec 17 14:44:23 2025 +0200
    
        Merge branch 'Yolo' of github.com:faustasgudas/PSP-pos-system-api into Yolo
    
    commit ed3c11e7542d95e424d9672fdab1437228826c25
    Author: faustasgudas <faustas.gudas@gmail.com>
    Date:   Wed Dec 17 14:44:04 2025 +0200
    
        discounts with eligible items
    
    commit d683cf4f2c31a89ec7cd8202b20e1b46bb39a9bf
    Merge: e0a0ed3 a8ea8bf
    Author: Matas <m.firantas@gmail.com>
    Date:   Tue Dec 16 20:36:49 2025 +0200
    
        Merge branch 'Yolo' of github.com:faustasgudas/PSP-pos-system-api into Yolo
    
    commit e0a0ed31ceb5a254d8cd958ceb57156c4830f231
    Merge: 986044e 3be5582
    Author: Matas <m.firantas@gmail.com>
    Date:   Tue Dec 16 19:38:05 2025 +0200
    
        Merge branch 'Yolo' of github.com:faustasgudas/PSP-pos-system-api into Yolo
    
    commit 986044e5fac138e842ebf427ca2e5993f28b8b37
    Merge: 20ff7f8 17fc1ac
    Author: Matas <m.firantas@gmail.com>
    Date:   Tue Dec 16 19:29:06 2025 +0200
    
        Merge branch 'main' of github.com:faustasgudas/PSP-pos-system-api into Yolo
    
    commit 20ff7f83afa20d62028e4fc59a793458acfe3e9f
    Merge: 260ef97 e2e0f1f
    Author: Matas <m.firantas@gmail.com>
    Date:   Tue Dec 16 18:51:40 2025 +0200
    
        Merge branch 'main' of github.com:faustasgudas/PSP-pos-system-api into Yolo

--- PsP/Services/Implementations/StripePaymentService.cs
+++ PsP/Services/Implementations/StripePaymentService.cs
@@ -60,15 +60,23 @@ public class StripePaymentService : IStripePaymentService
         CancellationToken ct = default)
     {
         var sessionService = new SessionService();
-        var session = await sessionService.GetAsync(stripeSessionId, cancellationToken: ct);
+        // Stripe.net does NOT auto-expand nested resources.
+        // If we don't expand "payment_intent", Session.PaymentIntent will be null even when PaymentIntentId exists.
+        var session = await sessionService.GetAsync(
+            stripeSessionId,
+            new SessionGetOptions { Expand = new List<string> { "payment_intent" } },
+            cancellationToken: ct);
 
-        if (session.PaymentIntent == null)
+        // Prefer expanded object; fall back to the id field if available.
+        var paymentIntentId = session.PaymentIntent?.Id ?? session.PaymentIntentId;
+
+        if (string.IsNullOrWhiteSpace(paymentIntentId))
             throw new InvalidOperationException("stripe_payment_intent_missing");
 
         var refundService = new RefundService();
         var options = new RefundCreateOptions
         {
-            PaymentIntent = session.PaymentIntent.Id, // 👈 čia ID
+            PaymentIntent = paymentIntentId,
             Amount        = amountCents
         };
 
